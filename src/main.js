47eb46bd980a233661e921d720069cc7397e98fd
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-04-14 16:47:37 +0900

    :heavy_plus_sign: infiniteScroll 의존성 추가
    - vue.js에서 제공하는 무한 스크롤링 관련 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -9,10 +9,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import { Carousel3d, Slide } from "vue-carousel-3d";
+import infiniteScroll from "vue-infinite-scroll";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(Carousel3d, Slide);
+Vue.use(infiniteScroll);
 
 Vue.config.productionTip = false;
 
