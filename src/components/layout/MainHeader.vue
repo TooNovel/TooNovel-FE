5069fb5c2a9ec82757eab434515a6af4944db053
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-04-14 16:46:13 +0900

    :lipstick: 헤더 로그인 위치 변경
    - 헤더의 로그인 버튼을 검색 기능 오른쪽으로 위치 변경

--- src/components/layout/MainHeader.vue
+++ src/components/layout/MainHeader.vue
@@ -6,35 +6,60 @@
     id="header"
   >
     <b-navbar-nav>
-      <b-navbar-brand href="/" tag="h1">ㅤTooNovel</b-navbar-brand>
-      <b-nav-item href="/login">Login</b-nav-item>
-      <b-nav-item href="/#">랭킹</b-nav-item>
-      <b-nav-item href="/#">리뷰</b-nav-item>
-      <b-nav-item href="/#">추천</b-nav-item>
-      <b-nav-item href="/#">작품찾기</b-nav-item>
-      <b-nav-item href="/community">커뮤니티</b-nav-item>
-      <b-nav-item href="/#">팬클럽</b-nav-item>
+      <b-navbar-brand style="font-size: 25px" href="/" tag="h1"
+        >ㅤTooNovel</b-navbar-brand
+      >
+      <b-nav-item style="font-size: 20px">랭킹</b-nav-item>
+      <b-nav-item style="font-size: 20px">리뷰</b-nav-item>
+      <b-nav-item style="font-size: 20px">추천</b-nav-item>
+      <b-nav-item style="font-size: 20px">작품찾기</b-nav-item>
+      <b-nav-item style="font-size: 20px" @click="getList()"
+        >커뮤니티</b-nav-item
+      >
+      <b-nav-item style="font-size: 20px">팬클럽</b-nav-item>
     </b-navbar-nav>
 
     <!-- Right aligned nav items -->
     <b-navbar-nav class="right">
       <b-nav-form class="mt-1" style="margin-right: 10px" id="searchForm">
-        <input
-          placeholder="작품을 검색하세요!"
-          v-model="search"
-          type="text"
-          v-on:keydown.enter.prevent=""
-          class="searchInput"
-        />
+        <input placeholder="작품을 검색하세요!" type="text" />
         <!-- <b-avatar class="searchIcon" icon="search"></b-avatar> -->
         <b-button size="sm" class="my-2 my-sm-0">검색</b-button>
+      </b-nav-form>
+      <b-nav-form class="mt-1">
+        <b-button
+          size="sm"
+          @click="goToLogin()"
+          id="loginBtn"
+          style="margin-right: 20px"
+          variant="primary"
+          >로그인</b-button
+        >
       </b-nav-form>
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      message: "MainHeader",
+    };
+  },
+  methods: {
+    goToLogin() {
+      this.$router.push("/login");
+    },
+
+    getList() {
+      this.$router.push("/community");
+    },
+  },
+  created() {
+    console.log("컴포넌트 생성!");
+  },
+};
 </script>
 
 <style scoped>
