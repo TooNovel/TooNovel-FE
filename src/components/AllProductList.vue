c596abcde7ad237bc67896fe9d1b53ffd55ba0d5
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-04-14 16:43:31 +0900

    :lipstick: 메인페이지 UI 개선
    - vue-carousel-3d를 이용해 작품 리스트들을 드래그로 이용해 조회 가능

---
+++ src/components/AllProductList.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <h3><b>전체 작품 리스트</b></h3>
+    <div id="allProductList">
+      <b-row>
+        <b-col
+          v-for="(product, index) in products"
+          :key="index"
+          class="mb-3"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+        >
+          <b-card>
+            <b-card-img
+              :src="product.img"
+              :alt="product.title"
+              @error="handleImageError"
+            ></b-card-img>
+            <b-card-title>{{ product.title }}</b-card-title>
+            <b-card-text>{{ product.contents }}</b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          title: "제목1",
+          contents: "내용1",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용2",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용3",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용4",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용5",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용6",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용7",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용8",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용9",
+          img: "https://example.com/image.jpg",
+        },
+        {
+          title: "제목1",
+          contents: "내용10",
+          img: "https://example.com/image.jpg",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleNotificationListScroll(e) {
+      const { scrollHeight, scrollTop, clientHeight } = e.target;
+      const isAtTheBottom = scrollHeight === scrollTop + clientHeight;
+      // 일정 이상 밑으로 내려오면 함수 실행 / 반복된 호출을 막기위해 1초마다 스크롤 감지 후 실행
+      if (isAtTheBottom) {
+        setTimeout(() => this.handleLoadMore(), 1000);
+      }
+    },
+
+    // 내려오면 api를 호출하여 아래에 더 추가,
+    handleLoadMore() {
+      console.log("리스트 추가");
+      // api를 호출하여 리스트 추가하면 됨, 현재는 pushList에 1개의 index 추가
+      this.pushList.push(2);
+    },
+
+    handleImageError(event) {
+      event.target.src =
+        "https://via.placeholder.com/600x600.png?text=No+Image";
+    },
+  },
+};
+</script>
+
+<style>
+#allProductList {
+  margin-top: 2%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+</style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,28 +1,57 @@
 <template>
-  <div>
-    <h1>주간 인기 작품</h1>
-    <carousel-3d>
-      <slide :index="0"> Slide 1 Content </slide>
-      <slide :index="1"> Slide 2 Content </slide>
-      <slide :index="2"> Slide 3 Content </slide>
-      <slide :index="3"> Slide 4 Content </slide>
-      <slide :index="4"> Slide 5 Content </slide>
-    </carousel-3d>
+  <div id="products">
+    <div>
+      <all-product></all-product>
+    </div>
+    <div>
+      <h3>주간 인기 작품</h3>
+      <carousel-3d>
+        <slide :index="0"> Slide 1 Content </slide>
+        <slide :index="1"> Slide 2 Content </slide>
+        <slide :index="2"> Slide 3 Content </slide>
+        <slide :index="3"> Slide 4 Content </slide>
+        <slide :index="4"> Slide 5 Content </slide>
+      </carousel-3d>
+    </div>
+    <div id="example">
+      <h3>신인 작가</h3>
+      <carousel-3d
+        :disable3d="true"
+        :space="365"
+        :clickable="false"
+        :controls-visible="true"
+      >
+        <slide :index="0"> Slide 1 Content </slide>
+        <slide :index="1"> Slide 2 Content </slide>
+        <slide :index="2"> Slide 3 Content </slide>
+        <slide :index="3"> Slide 4 Content </slide>
+        <slide :index="4"> Slide 5 Content </slide>
+      </carousel-3d>
+    </div>
   </div>
 </template>
 
 <script>
 import { Carousel3d, Slide } from "vue-carousel-3d";
+import AllProduct from "./AllProductList.vue";
 
 export default {
   name: "MainPage",
   data() {
-    return {};
+    return {
+      slides: 5,
+    };
   },
   components: {
     Carousel3d,
     Slide,
+    "all-product": AllProduct,
   },
   methods: {},
 };
 </script>
+<style>
+#products {
+  margin: 5%;
+}
+</style>
