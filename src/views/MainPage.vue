7b30cab8ae4ceede11c2cd89ec6c80b1ba92eaf4
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-04-19 20:06:01 +0900

    :truck: 파일 구조 위치 변경
    - header, footer를 제외한 나머지 파일 views 폴더로 경로 이동

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -6,11 +6,14 @@
     <div>
       <h3>주간 인기 작품</h3>
       <carousel-3d>
-        <slide :index="0"> Slide 1 Content </slide>
-        <slide :index="1"> Slide 2 Content </slide>
-        <slide :index="2"> Slide 3 Content </slide>
-        <slide :index="3"> Slide 4 Content </slide>
-        <slide :index="4"> Slide 5 Content </slide>
+        <slide
+          v-for="(product, index) in products"
+          :key="index"
+          :index="index - 1"
+          style="width: 250px; height: 300px"
+        >
+          <img :src="product.image" width="250px" height="300px" />
+        </slide>
       </carousel-3d>
     </div>
     <div id="example">
@@ -26,12 +29,18 @@
         <slide :index="2"> Slide 3 Content </slide>
         <slide :index="3"> Slide 4 Content </slide>
         <slide :index="4"> Slide 5 Content </slide>
+        <slide :index="5"> Slide 1 Content </slide>
+        <slide :index="6"> Slide 2 Content </slide>
+        <slide :index="7"> Slide 3 Content </slide>
+        <slide :index="8"> Slide 4 Content </slide>
+        <slide :index="9"> Slide 5 Content </slide>
       </carousel-3d>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import { Carousel3d, Slide } from "vue-carousel-3d";
 import AllProduct from "./AllProductList.vue";
 
@@ -39,18 +48,30 @@ export default {
   name: "MainPage",
   data() {
     return {
-      slides: 5,
+      products: [],
     };
+  },
+  mounted() {
+    const URL = "https://aee2de78-332d-40e2-af1e-ee62eb99672d.mock.pstmn.io";
+
+    axios
+      .get(URL + "/work/readall")
+      .then((response) => {
+        this.products = response.data.products;
+        console.log(response.data.products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
   components: {
     Carousel3d,
     Slide,
     "all-product": AllProduct,
   },
-  methods: {},
 };
 </script>
-<style>
+<style scoped>
 #products {
   margin: 5%;
 }
--- src/views/WorkViewPage.vue
+++ src/views/WorkViewPage.vue
@@ -1,53 +1,52 @@
 <template>
-    <div id="community">
-      <h1>작품</h1>
-      <br />
-      <br />
-      <table>
-        <colgroup>
-          <col width="33%" />
-          <col width="33%" />
-          <col width="33%" />
-        </colgroup>
-        <thead>
-          <tr>
-            <th>index</th>
-            <th>제목</th>
-            <th>이미지</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="work in works" :key="{ work }">
-            <td>{{ work.work_id }}</td>
-            <td>{{ work.title }}</td>
-            <td><img :src="work.image" /></td>
-          </tr>
-        </tbody>
-      </table>
-      <b-button variant="outline-primary">
-        <a href="/community/write">글쓰기</a>
-      </b-button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        works: [],
-      };
-    },
-    created() {
-      this.posts = this.$route.params.data;
-    },
-  };
-  </script>
-  <style>
-  #community {
-    margin: 3%;
-  }
-  table {
-    width: 100%;
-  }
-  </style>
-  
+  <div id="community">
+    <h1>작품</h1>
+    <br />
+    <br />
+    <table>
+      <colgroup>
+        <col width="33%" />
+        <col width="33%" />
+        <col width="33%" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th>index</th>
+          <th>제목</th>
+          <th>이미지</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="work in works" :key="{ work }">
+          <td>{{ work.work_id }}</td>
+          <td>{{ work.title }}</td>
+          <td><img :src="work.image" /></td>
+        </tr>
+      </tbody>
+    </table>
+    <b-button variant="outline-primary">
+      <a href="/community/write">글쓰기</a>
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      works: [],
+    };
+  },
+  created() {
+    this.posts = this.$route.params.data;
+  },
+};
+</script>
+<style scoped>
+#community {
+  margin: 3%;
+}
+table {
+  width: 100%;
+}
+</style>
