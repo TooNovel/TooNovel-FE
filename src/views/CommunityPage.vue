83160dde0548034401d10192cebd8e13e1857be5
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-05-03 11:02:51 +0900

    :lipstick: 커뮤니티 페이지 UI 개선

--- src/views/CommunityPage.vue
+++ src/views/CommunityPage.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="community">
-    <h1>커뮤니티</h1>
+    <header>
+      <div class="row justify-content-end">
+        <div class="col">
+          <h1>커뮤니티</h1>
+        </div>
+        <div class="col-auto">
+          <b-button size="lg" href="/community/write">글쓰기</b-button>
+        </div>
+      </div>
+    </header>
     <br />
-    <div>
-      <b-button pill>전체</b-button>
-      <b-button pill>자유</b-button>
-      <b-button pill>장르</b-button>
-      <b-button pill>질문</b-button>
-      <b-button pill>이벤트</b-button>
-    </div>
-    <br />
-    <!-- <tr v-for="post in posts" :key="{ post }">
-            <td>{{ post.postId }}</td>
-            <td>{{ post.title }}</td>
-            <td>{{ post.authorName }}</td>
-            <td>{{ post.viewCount }}</td>
-            <td>{{ post.createDate }}</td>
-        </tr> -->
-    <b-button variant="outline-primary">
-      <a href="/community/write">글쓰기</a>
-    </b-button>
+    <section>
+      <div>
+        <b-nav tabs align="center">
+          <b-nav-item>자유</b-nav-item>
+          <b-nav-item>장르</b-nav-item>
+          <b-nav-item>질문</b-nav-item>
+          <b-nav-item>이벤트</b-nav-item>
+        </b-nav>
+      </div>
+      <br />
+      <b-table hover :fields="fields"> </b-table>
+    </section>
   </div>
 </template>
 
@@ -28,6 +31,24 @@ export default {
   data() {
     return {
       posts: [],
+      fields: [
+        {
+          key: "category",
+          label: "카테고리",
+        },
+        {
+          key: "title",
+          label: "제목",
+        },
+        {
+          key: "author",
+          label: "작성자",
+        },
+        {
+          key: "createdDate",
+          label: "작성일자",
+        },
+      ],
     };
   },
   created() {
@@ -38,5 +59,14 @@ export default {
 <style scoped>
 #community {
   margin: 3%;
+}
+header {
+  background-color: white;
+  font-family: Hanna;
+  padding: 1rem;
+}
+
+section {
+  background-color: white;
 }
 </style>
