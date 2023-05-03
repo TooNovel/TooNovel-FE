ee8bcab3757c6758388344d634bdc30ea42c514e
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-05-03 11:07:13 +0900

    :lipstick: 메인 페이지 UI 개선

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -3,20 +3,42 @@
     <div>
       <all-product></all-product>
     </div>
-    <div>
+    <br />
+    <div v-if="products.length > 0">
       <h3>주간 인기 작품</h3>
-      <carousel-3d>
+      <carousel-3d
+        :pagination="true"
+        :controls-visible="true"
+        :pagination-custom="paginationCustom"
+        ref="carousel"
+      >
         <slide
           v-for="(product, index) in products"
           :key="index"
-          :index="index - 1"
+          :index="index"
           style="width: 250px; height: 300px"
         >
           <img :src="product.image" width="250px" height="300px" />
         </slide>
       </carousel-3d>
     </div>
-    <div id="example">
+    <div>
+      <h3>인기 상승 중인 작품</h3>
+      <carousel-3d
+        :disable3d="true"
+        :space="365"
+        :clickable="false"
+        :controls-visible="true"
+      >
+        <slide :index="0"> Slide 1 Content </slide>
+        <slide :index="1"> Slide 1 Content </slide>
+        <slide :index="2"> Slide 1 Content </slide>
+        <slide :index="3"> Slide 1 Content </slide>
+        <slide :index="4"> Slide 1 Content </slide>
+      </carousel-3d>
+    </div>
+    <br />
+    <div>
       <h3>신인 작가</h3>
       <carousel-3d
         :disable3d="true"
@@ -25,17 +47,29 @@
         :controls-visible="true"
       >
         <slide :index="0"> Slide 1 Content </slide>
-        <slide :index="1"> Slide 2 Content </slide>
-        <slide :index="2"> Slide 3 Content </slide>
-        <slide :index="3"> Slide 4 Content </slide>
-        <slide :index="4"> Slide 5 Content </slide>
-        <slide :index="5"> Slide 1 Content </slide>
-        <slide :index="6"> Slide 2 Content </slide>
-        <slide :index="7"> Slide 3 Content </slide>
-        <slide :index="8"> Slide 4 Content </slide>
-        <slide :index="9"> Slide 5 Content </slide>
+        <slide :index="1"> Slide 1 Content </slide>
+        <slide :index="2"> Slide 1 Content </slide>
+        <slide :index="3"> Slide 1 Content </slide>
+        <slide :index="4"> Slide 1 Content </slide>
       </carousel-3d>
     </div>
+    <br />
+    <div>
+      <h3>주간 인기 리뷰</h3>
+      <carousel-3d
+        :disable3d="true"
+        :space="365"
+        :clickable="false"
+        :controls-visible="true"
+      >
+        <slide :index="0"> Slide 1 Content </slide>
+        <slide :index="1"> Slide 1 Content </slide>
+        <slide :index="2"> Slide 1 Content </slide>
+        <slide :index="3"> Slide 1 Content </slide>
+        <slide :index="4"> Slide 1 Content </slide>
+      </carousel-3d>
+    </div>
+    <br />
   </div>
 </template>
 
@@ -46,13 +80,8 @@ import AllProduct from "./AllProductList.vue";
 
 export default {
   name: "MainPage",
-  data() {
-    return {
-      products: [],
-    };
-  },
-  mounted() {
-    const URL = "https://aee2de78-332d-40e2-af1e-ee62eb99672d.mock.pstmn.io";
+  created() {
+    const URL = process.env.VUE_APP_TEST_URL;
 
     axios
       .get(URL + "/work/readall")
@@ -64,10 +93,36 @@ export default {
         console.log(error);
       });
   },
+  data() {
+    return {
+      products: [],
+      paginationCustom: {
+        style: {
+          backgroundColor: "#fff",
+          borderRadius: "50%",
+          color: "#000",
+          fontSize: "16px",
+          margin: "0 5px",
+          padding: "5px",
+          width: "30px",
+          height: "30px",
+        },
+        activeStyle: {
+          backgroundColor: "#000",
+          color: "#fff",
+        },
+      },
+    };
+  },
   components: {
     Carousel3d,
     Slide,
     "all-product": AllProduct,
+  },
+  methods: {
+    goToSlide(index) {
+      this.$refs.carousel.goToSlide(index);
+    },
   },
 };
 </script>
