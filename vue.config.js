39d154c7ee9dfec60660501f9f3f8b29293776e1
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-05-03 11:12:04 +0900

    :wrench: 환경 변수 변경

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,11 @@
 require('dotenv').config();
-
 module.exports = {
-    devServer: {
-        proxy: {
-            "/work": {
-                target: process.env.VUE_APP_API_URL,
-                changeOrigin: true,
-            },
-            "/post": {
-                target: process.env.VUE_APP_API_URL,
-                changeOrigin: true,
-            }
-        }
-    }
-}
+  devServer: {
+    proxy: {
+      "/api": {
+        target: process.env.VUE_APP_API_URL,
+        changeOrigin: true,
+      },
+    },
+  },
+};
