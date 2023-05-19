a456512847dcb862d619bd18f950e331f37dfe1a
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-05-19 14:04:38 +0900

    :sparkles: 소설 좋아요 기능 구현
    - 각 작품마다 좋아요 기능 구현
    - 유저 마다 좋아요 상태 다름

--- src/components/NovelLike.vue
+++ src/components/NovelLike.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="row">
     <div>
-      <button @click="novelLikeBtn()" :class="{ active: novelLiked }">
+      <button @click="novelLike(novel)" :class="{ active: novelLiked }">
         <img
           :src="
             novelLiked
               ? 'https://cdn-icons-png.flaticon.com/512/803/803087.png'
               : 'https://cdn-icons-png.flaticon.com/512/812/812327.png'
           "
-          :alt="novelLiked ? '좋아요 완료' : '좋아요 하기'"
           width="24"
           height="24"
         />
@@ -19,20 +18,56 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      novels: {},
       novelLiked: false,
+      accessToken: this.$store.getters.getAccessToken,
     };
   },
   props: ["novel"],
+  async created() {
+    try {
+      const novelId = this.$route.params.novel_id;
+      const option = {
+        headers: {
+          Authorization: "Bearer " + this.accessToken,
+        },
+      };
+      const result = await axios.get(
+        "/api/v1/novel/" + novelId + "/like",
+        option
+      );
+      console.log(result);
+      this.novelLiked = result.data.like;
+    } catch (err) {
+      console.log(err);
+    }
+  },
   methods: {
-    novelLikeBtn() {
-      this.novelLiked = !this.novelLiked;
+    async novelLike(res) {
+      if (this.accessToken == null || this.accessToken == "") {
+        alert("로그인 후 이용 가능 합니다!");
+        return;
+      }
+      try {
+        const novelId = res.novelId;
+        const option = {
+          headers: {
+            Authorization: "Bearer " + this.accessToken,
+          },
+        };
+        await axios.put("/api/v1/novel/" + novelId + "/like", null, option);
+        const result = await axios.get("/api/v1/novel/" + novelId, option);
+        this.novelLiked = !this.novelLiked;
+        res.likeCount = result.data.likeCount;
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped></style>
