14f6afa953e7b5847619ae28842f14d3ed209744
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-06-08 10:42:41 +0900

    :recycle: 메인 헤더 수정
    - 전체 리뷰 조회 페이지와 전체 작품 페이지로 이동 시 전달되는 params 값 제외

--- src/components/layout/MainHeader.vue
+++ src/components/layout/MainHeader.vue
@@ -90,10 +90,8 @@ export default {
   methods: {
     async getAllReview() {
       try {
-        const res = await axios.get("/api/v1/review");
         this.$router.push({
           name: "reviews",
-          params: { data: res.data },
         });
       } catch (err) {
         console.log(err);
@@ -101,11 +99,8 @@ export default {
     },
     async getAllNovel() {
       try {
-        const res = await axios.get("/api/v1/novel");
-        localStorage.setItem("novelList", JSON.stringify(res.data));
         this.$router.push({
           name: "novels",
-          params: { data: res.data },
         });
       } catch (err) {
         console.log(err);
