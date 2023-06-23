db028ea7472e2788bfd3ade76034f6c02921b754
kdk8672 <kdk8672@naver.com>
2023-06-23 16:53:20 +0900

    ♻️ 푸터 변경 - 필요 없는 문구 제거 - 위치 조정

--- src/components/layout/MainFooter.vue
+++ src/components/layout/MainFooter.vue
@@ -2,43 +2,6 @@
   <div class="footer-dark">
     <footer>
       <div class="container">
-        <div class="row">
-          <div class="col-sm-6 col-md-3 item">
-            <h3>장르</h3>
-            <ul>
-              <li><a href="#">로맨스</a></li>
-              <li><a href="#">로판</a></li>
-              <li><a href="#">판타지</a></li>
-              <li><a href="#">현판</a></li>
-              <li><a href="#">무협</a></li>
-              <li><a href="#">미스테리</a></li>
-              <li><a href="#">역사</a></li>
-              <li><a href="#">SF</a></li>
-            </ul>
-          </div>
-          <div class="col-sm-6 col-md-3 item">
-            <h3>기타</h3>
-            <ul>
-              <li><a href="#">작품찾기</a></li>
-              <li><a href="#">커뮤니티</a></li>
-              <li><a href="#">팬클럽</a></li>
-            </ul>
-          </div>
-          <div class="col-sm-6 col-md-3 item">
-            <h3>계정</h3>
-            <ul>
-              <li>
-                <a href="#">로그인</a>
-              </li>
-              <li><a href="#">회원가입</a></li>
-            </ul>
-          </div>
-          <div class="col item social">
-            <a><b-icon-instagram></b-icon-instagram></a>
-            <a><b-icon-facebook></b-icon-facebook></a>
-            <a><b-icon-twitter></b-icon-twitter></a>
-          </div>
-        </div>
         <p class="copyright">
           감성과 이야기가 어우러지는 투노벨, 작가와 독자의 새로운 만남 © 2023
         </p>
@@ -52,7 +15,11 @@ export default {};
 
 <style scoped>
 .footer-dark {
-  padding: 50px 0;
+  padding: 3%;
+}
+
+footer {
+  margin-top: 0;
 }
 
 .footer-dark h3 {
@@ -131,7 +98,6 @@ export default {};
 
 .footer-dark .copyright {
   text-align: center;
-  padding-top: 24px;
   opacity: 0.3;
   font-size: 17px;
   margin-bottom: 0;
