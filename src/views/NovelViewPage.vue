9516601fcbf734f9500230434aa3375b9bdb2bd0
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-06-24 10:50:40 +0900

    :lipstick: 소설 전체 조회 CSS
    - 메뉴바와 정렬순 버튼 추가
    - 디자인 개선

--- src/views/NovelViewPage.vue
+++ src/views/NovelViewPage.vue
@@ -1,10 +1,36 @@
 <template>
   <div class="container">
-    <h3 class="title"><b>전체 작품 리스트</b></h3>
     <div id="loading" v-if="isLoading" style="height: 800px">
       <div class="loader">Loading...</div>
     </div>
     <div v-else class="novel-list-box" ref="allProductList">
+      <b-row class="menu-bar-parent">
+        <ul class="menu-bar">
+          <div v-for="genre in genres" :key="genre.key">
+            <div @click="getGenreRanking(genre.key)">
+              <li>{{ genre.value }}</li>
+            </div>
+          </div>
+        </ul>
+      </b-row>
+      <div class="sort-row">
+        <div v-for="sort in sorts" :key="sort.key" class="sort-box">
+          <div class="iconList" @click="getSortRanking(sort.key)">
+            <div v-if="sort.key == 'NOVEL_LIKE_DESC'">
+              <h5>
+                <b-icon icon="hand-thumbs-up"></b-icon>&nbsp;{{ sort.value }}
+              </h5>
+            </div>
+            <div v-else-if="sort.key == 'NOVEL_GRADE_DESC'">
+              <h5><b-icon icon="star"></b-icon>&nbsp;{{ sort.value }}</h5>
+            </div>
+            <div v-else-if="sort.key == 'NOVEL_REVIEW_DESC'">
+              <h5><b-icon icon="chat-text"></b-icon>&nbsp;{{ sort.value }}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
       <b-row>
         <b-col
           v-for="(novel, index) in novels"
@@ -17,15 +43,39 @@
         >
           <b-card @click="detailNovelList(novel.novelId)">
             <b-card-img :src="novel.image" class="card-image"></b-card-img>
-            <b-card-title>{{ novel.title }}</b-card-title>
-            <b-card-text>{{ novel.author }}</b-card-text>
-            <b-card-text>{{ novel.genre }}</b-card-text>
-            <b-card-text>좋아요 {{ novel.likeCount }}</b-card-text>
-            <b-card-text
-              >{{ novel.grade ? novel.grade : 0 }} ({{
-                novel.reviewCount
-              }})</b-card-text
+            <b-card-text id="novel-text"
+              >{{ novel.genre }} | {{ novel.author }}</b-card-text
             >
+            <b-card-title>{{ novel.title }}</b-card-title>
+            <template #footer>
+              <div
+                style="
+                  display: flex;
+                  justify-content: space-between;
+                  align-items: center;
+                "
+              >
+                <star-rating
+                  :border-width="4"
+                  border-color="#d8d8d8"
+                  :rounded-corners="true"
+                  :star-size="20"
+                  v-model="novel.grade"
+                  read-only
+                  :star-points="[
+                    23, 2, 14, 17, 0, 19, 10, 34, 7, 50, 23, 43, 38, 50, 36, 34,
+                    46, 19, 31, 17,
+                  ]"
+                ></star-rating
+                >&nbsp;({{ novel.reviewCount }})&nbsp;&nbsp;
+                <b-icon
+                  icon="hand-thumbs-up"
+                  font-scale="1.5"
+                  variant="danger"
+                ></b-icon>
+                {{ novel.likeCount }}
+              </div>
+            </template>
           </b-card>
         </b-col>
       </b-row>
@@ -41,6 +91,7 @@
 <script>
 import axios from "axios";
 import InfiniteLoading from "vue-infinite-loading";
+import StarRating from "vue-star-rating";
 
 export default {
   data() {
@@ -48,6 +99,25 @@ export default {
       novels: [],
       novelId: null,
       isLoading: true,
+      novelGrade: 0,
+      genre: "",
+      sort: "",
+      genres: [
+        { key: "top30", value: "top30" },
+        { key: "FANTASY", value: "판타지" },
+        { key: "ROMANCE", value: "로맨스" },
+        { key: "ROMANCE_FANTASY", value: "로맨스판타지" },
+        { key: "MODERN_FANTASY", value: "현대판타지" },
+        { key: "WUXIA", value: "무협" },
+        { key: "MYSTERY", value: "미스테리" },
+        { key: "LIGHT_NOVEL", value: "라이트노벨" },
+        { key: "BL", value: "BL" },
+      ],
+      sorts: [
+        { key: "NOVEL_LIKE_DESC", value: "좋아요 순" },
+        { key: "NOVEL_GRADE_DESC", value: "평점 순" },
+        { key: "NOVEL_REVIEW_DESC", value: "리뷰개수 순" },
+      ],
     };
   },
   async created() {
@@ -55,6 +125,32 @@ export default {
     this.isLoading = false;
   },
   methods: {
+    async getGenreRanking(genre) {
+      console.log(genre);
+      if (genre === "top30") {
+        genre = "";
+      }
+      this.genre = genre;
+      try {
+        const res = await axios.get(
+          `${process.env.VUE_APP_API_URL}/novel?genre=${this.genre}&sort=${this.sort}`
+        );
+        this.novels = res.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async getSortRanking(sort) {
+      this.sort = sort;
+      try {
+        const res = await axios.get(
+          `${process.env.VUE_APP_API_URL}/novel?genre=${this.genre}&sort=${this.sort}`
+        );
+        this.novels = res.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
     async infiniteHandler($state) {
       if (!this.novels.length) {
         // 데이터가 없는 경우 초기 데이터를 가져옵니다.
@@ -107,13 +203,68 @@ export default {
   },
   components: {
     InfiniteLoading,
+    StarRating,
   },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.menu-bar-parent {
+  margin-top: 50px;
+  margin-bottom: 25px;
+  display: flex;
+  justify-content: center;
+}
+.menu-bar {
+  border-radius: 25px;
+  height: fit-content;
+  display: inline-flex;
+  background-color: rgb(130, 176, 130);
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  align-items: center;
+  padding: 0 10px;
+  width: 73%;
+  li {
+    list-style: none;
+    color: white;
+    padding: 12px 16px;
+    margin: 0 8px;
+    position: relative;
+    cursor: pointer;
+    white-space: nowrap;
+    &::before {
+      content: " ";
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      z-index: -1;
+      transition: 0.2s;
+      border-radius: 25px;
+    }
+    &:hover {
+      &::before {
+        background: linear-gradient(to bottom, #e8edec, #d2d1d3);
+        box-shadow: 0px 3px 20px 0px black;
+        transform: scale(1.2);
+      }
+      color: black;
+    }
+  }
+}
+.sort-row {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-end;
+}
 #allProductList {
   height: 400px; /* 스크롤이 표시될 최대 높이값 */
+}
+p {
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 .title {
   margin: 20px;
@@ -123,7 +274,14 @@ export default {
   margin-left: 5%;
   margin-right: 5%;
 }
+.card-body {
+  height: 420px;
+}
 .card-image {
   height: 300px;
+}
+.iconList {
+  color: gray;
+  margin-right: 10px;
 }
 </style>
